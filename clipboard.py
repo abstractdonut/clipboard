cc663c472acb64972426993014dca0da9fe7f341
Micah Borden <abstractdonut@gmail.com>
2023-07-07 14:09:02 -0700

    Update clipboard.py

--- clipboard.py
+++ clipboard.py
@@ -3,11 +3,13 @@ from pynput import keyboard
 
 import boto3
 
+
 # Set your AWS access key ID and secret access key
 AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
 bucket = "globalclipboard"
 objkey = "comm_chan"
+
 
 # Create an S3 client
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
@@ -23,19 +25,18 @@ def read_s3_file(bucket_name=bucket, object_key=objkey):
         return content
 
     except Exception as e:
-        
         print(f"Error: {e}")
 
 
 
 def overwrite_s3_file(content, bucket_name=bucket, object_key=objkey):
+    
     try:
         
         s3.put_object(Body=content.encode('utf-8'), Bucket=bucket_name, Key=object_key)
         print(f"File '{object_key}' in bucket '{bucket_name}' overwritten successfully.")
 
     except Exception as e:
-        
         print(f"Error: {e}")
 
 
@@ -44,15 +45,12 @@ def overwrite_s3_file(content, bucket_name=bucket, object_key=objkey):
 # See https://github.com/moses-palmer/pynput/issues/20
 alt_pressed = False
 
-
 def on_press(key):
     
     global alt_pressed
-        
     
     if key == keyboard.Key.alt:
         alt_pressed = True
-    
     
     # Check if the read combination has been pressed.
     try:
@@ -68,7 +66,6 @@ def on_press(key):
             overwrite_s3_file(clipboard_content)
     
     except AttributeError:
-        
         pass
 
 
@@ -79,7 +76,6 @@ def on_release(key):
     
     if key == keyboard.Key.alt:
         alt_pressed = False
-    
     
     if key == keyboard.Key.esc:
         print("Stop event detected.")
@@ -93,7 +89,6 @@ if __name__ == "__main__":
     print("Press 'Alt + r' to read from the global clipboard into your clipboard.")
     print("Press 'Alt + w' to write the contents of your clipboard into the global clipboard.")
     print("Press 'Esc' to exit.")
-    
     
     # Create a listener for keyboard events
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
